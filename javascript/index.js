b897b480667048e84e583bea843a4a38169e1fdc
Michael Monteblanco <michael.thomas.monteblanco@hotmail.com>
2024-09-22 22:56:39 -0700

    Added index html and index js

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+/* This si to add an eventlistener to the buttons in the index page
+<button id="startGameButton">Start Game!</button>
+<script>
+    document.getElementById('startGameButton').addEventListener('click', function() {
+        // Perform any actions you need here
+        window.location.href = 'game.html'; // Navigate to the game page
+    });
+</script>
+*/ 
+/* Comnsiderations for this page:
+        -Add animations to enahnce user experience
+        -Adding a way to keep track of game's score, consider using js or localStorage
+        -localStorage
+        -Navigation handling
+*/
